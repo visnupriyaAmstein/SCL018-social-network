0ba2d32270a350091390f14e33615ff2fcfe979d
Maferamosp <ramosmariaf@hotmail.com>
2021-11-02 10:55:35 -0300

    creacion maqueta inicial

---
+++ src/components/wallPage.js
@@ -0,0 +1,17 @@
+/*import introPage from './introPage.js';
+
+export const wallPage = () => {
+  const wallContainer = document.createElement('main');
+  wallContainer.className = 'wallContainer';
+  wallContainer.id = 'wallContainer';
+
+  const header = document.createElement('wallHeader');
+  header.className = 'wallHeader';
+  wallContainer.appendChild(header);
+
+  const logo = document.createElement('logoImage');
+  logo.className = 'logoImage';
+  logo.src = 'images/logo-petsbook.png';
+
+  return wallContainer + introPage;
+};
