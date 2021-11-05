33b2205e1913742e2f4eac1817dea82b637a65be
Maferamosp <ramosmariaf@hotmail.com>
2021-11-05 09:49:56 -0300

    login

--- src/components/registerPage.js
+++ src/components/registerPage.js
@@ -1,22 +1,28 @@
-// export const registerPage = () => {
-//   const register = document.createElement('section');
-//   register.className = 'modal';
-//   register.innerHTML = `
-//     <div class="container">
-//     <div class="container-register">
-//       <h1>REGISTRATE</h1>
-//     </div>
-//     <div class="input-container">
-//       <input type="text" id="nameRegister" placeholder="Nombre">
-//      <input type="email" id="mailRegister" placeholder="correo@example.com">
-//       <input type="password" id="passwordRegister" placeholder="contraseña">
-//     </div>
-//     <div class="boton-register">
-//       <button id="btnRegister" class="boton-register">Crear Cuenta</button>
-//     </div>
-//   </div>
+import { userRegister } from "../lib/index.js";
 
-// `;
+const root = document.getElementById("root");
+export const registerPage = () => {
+  const register = document.createElement('section');
+  register.className = 'modal';
+  register.innerHTML = `
+    <div class="container">
+    <div class="container-register">
+      <h1>REGISTRATE</h1>
+    </div>
+    <div class="input-container">
+      <input type="text" id="nameRegister" placeholder="Nombre">
+     <input type="email" id="mailRegister" placeholder="correo@example.com">
+      <input type="password" id="passwordRegister" placeholder="contraseña">
+    </div>
+    <div class="boton-register">
+      <button id="btnRegister" class="boton-register">Crear Cuenta</button>
+    </div>
+  </div>
+`;
+root.appendChild(register);
 
-// return registerPage;
-// }
+document.querySelector("#btnRegister").addEventListener("click", ()=>{
+  userRegister();
+});
+return registerPage;
+}
--- src/paths.js
+++ src/paths.js
@@ -1,14 +1,18 @@
-// import { introPage } from "./components/introPage.js";
+import { introPage } from "./components/introPage.js";
+import { registerPage } from "./components/registerPage.js"; 
 
-// export const routes = (hash) => {
-//   const rootContainer = document.getElementById("root");
-//   rootContainer.innerHTML = "";
-//   if (hash === "#/" || hash === "/" || hash === "#" || hash === "") {
-//     rootContainer.appendChild(introPage());
-//   } else if (hash === "#/introPage") {
-//     rootContainer.appendChild(introPage());
-//   }
-// };
+export const routes = (hash) => {
+  const rootContainer = document.getElementById("root");
+  rootContainer.innerHTML = "";
+  if (hash === "#/" || hash === "/" || hash === "#" || hash === "") {
+    rootContainer.appendChild(introPage());
+  } else if (hash === "#/introPage") {
+    rootContainer.appendChild(introPage());
+  } else if (hash === "#/registerPage"){
+    rootContainer.appendChild(registerPage());
+  }
+};
+
 
 /*export const routes = (hash) => {
     const root = document.getElementById('root');
