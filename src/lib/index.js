6b7addf6020ff3f6961c703bf5e6dd1df421c57f
Maferamosp <ramosmariaf@hotmail.com>
2021-11-15 10:08:45 -0300

    funcionalidad de los post

--- src/lib/index.js
+++ src/lib/index.js
@@ -7,15 +7,10 @@ import {
   GoogleAuthProvider,
   signInWithRedirect,
   getRedirectResult,
-  // signOut,
-  // onAuthStateChanged
+  signOut,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-} from "https://www.gstatic.com/firebasejs/9.2.0/firebase-firestore.js";
+import {getFirestore,collection,addDoc,getDocs } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -34,39 +29,55 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider(app);
 const db = getFirestore();
 // const user = auth.currentUser;
+// console.log(user);
 console.log(app);
 
-export const userRegister = (email, password) => {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-      console.log("creado");
-      window.location.hash = "#/introPage";
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-      console.log(errorCode + errorMessage);
-    });
-};
 
-export const userLogin = (email1, password1) => {
-  signInWithEmailAndPassword(auth, email1, password1)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-      console.log("logged in");
-      window.location.hash = "#/wallPage";
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode + errorMessage);
-    });
+export const userRegister = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        console.log("creado");
+        window.location.hash = "#/introPage";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        console.log(errorCode + errorMessage);
+      });
+};
+export const onAuth = () =>{
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      console.log(uid)
+    } else {
+      // User is signed out
+      // logOut();
+      console.log("no existe user")
+      logOut();
+    }
+  });
+  }
+export const userLogin = (email1,password1) => {
+    signInWithEmailAndPassword(auth, email1, password1)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        console.log("logged in");
+        window.location.hash = "#/wallPage"
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode + errorMessage);
+      });
 };
 export const loginWithGoogle = () => {
   signInWithRedirect(auth, provider);
@@ -93,46 +104,31 @@ export const loginWithGoogle = () => {
     });
 };
 
-// export const logOut = () =>{
-//   document.querySelector("#logOut").addEventListener("click", () =>{
-// signOut(auth).then(() => {
-//   console.log(signOut()) // Sign-out successful.
-//   window.location.hash = "#/introPage"
-// }).catch((error) => {
-//   // An error happened.
-// });
-// })
-// }
-// export const onAuth = () =>{
-// onAuthStateChanged(auth, (user) => {
-//   if (user) {
-//     // User is signed in, see docs for a list of available properties
-//     // https://firebase.google.com/docs/reference/js/firebase.User
-//     const uid = user.uid;
-//     console.log(uid)
-//   } else {
-//     // User is signed out
-//     window.location.hash = "#/introPage"
-//   }
-// });
-// }
+export const logOut = () =>{
+  const auth = getAuth();
+  signOut(auth).then(() => {
+    window.location.hash = "#/";// Sign-out successful.
+  }).catch((error) => {
+    console.log(error);// An error happened.
+  });
+  }
+
 export const addData = async (postInput) => {
+  console.log(postInput);
   try {
     const docRef = await addDoc(collection(db, "posts"), {
       posts: postInput
-
     });
     console.log("Document written with ID: ", docRef.id);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 }
-export const readData = async () => {
-  
+// addData("posts");
+export const readData = async () => { 
   const querySnapshot = await getDocs(collection(db, "posts"));
 querySnapshot.forEach((doc) => {
   console.log(`${doc.id} => ${doc.data().posts}`);
 });
 }
-
 readData();
--- src/paths.js
+++ src/paths.js
@@ -1,6 +1,7 @@
 import { introPage } from "./components/introPage.js";
 import { registerPage } from "./components/registerPage.js";
 import { wallPage } from "./components/wallPage.js";
+import { postsModal } from "./components/postsModal.js";
 //import { onAuth } from "../lib/index.js"
 
 export const routes = (hash) => {
@@ -14,5 +15,8 @@ export const routes = (hash) => {
     rootContainer.appendChild(registerPage());
   } else if (hash === "#/wallPage") {
     rootContainer.appendChild(wallPage());
+  } else if (hash === "#/post") {
+    rootContainer.appendChild(postsModal());
   }
+
 };
